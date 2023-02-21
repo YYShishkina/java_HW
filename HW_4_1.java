223a5d55365bae3d7011e002df1c2208fb874218
Viktor Shishkin <viktorshishkin@MacBook-Air-Viktor.local>
2023-02-21 19:42:43 +0300

    Добавлено решение 4-го ДЗ

---
+++ HW_4_1.java
@@ -0,0 +1,62 @@
+//1. Реализовать консольное приложение, которое:
+//        Принимает от пользователя и “запоминает” строки.
+//        Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+//        Если введено revert, удаляет предыдущую введенную строку из памяти.
+
+package HW_1;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+
+public class HW_4_1 {
+    public static void main (String [] args) {
+        String str;
+        LinkedList <String> lst = new LinkedList<String>();
+        int leng = 0;
+        while (true) {
+            str = EnterString();
+            leng = lst.size();
+            if (str.equals("print")) {
+                Print(lst);
+            }
+            if (str.equals("revert") & (leng!=0))  {
+                Revert(lst);
+            }
+
+            if (str.equals("revert") & (leng==0)) {
+                System.out.println ("The list is alredy empty");
+            }
+            if ((str.equals("print")==false) & (str.equals("revert")==false)){
+               saveNew(str, lst);
+            }
+            if (str.equals("exit")) {
+                break;
+            }
+        }
+    }
+
+    public static String EnterString () {
+        Scanner str = new Scanner(System.in);
+        System.out.print("Enter string - ");
+        return (str.nextLine());
+    }
+
+    public static LinkedList<String> saveNew (String str, LinkedList lst) {
+        lst.addFirst(str);
+        return lst;
+    }
+
+    public static void Print (LinkedList lst) {
+        System.out.println (lst);
+    }
+
+    public static void Revert (LinkedList lst) {
+        lst.removeFirst();
+    }
+
+}
+
+
+
+
---
+++ HW_4_2.java
@@ -0,0 +1,38 @@
+
+
+//2. Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+
+
+package HW_1;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+
+
+public class HW_4_1 {
+    public static void main (String [] args) {
+        LinkedList<String> lst = new LinkedList();
+        String str = "1qwerty";
+        String str2 = "2qwerty";
+        String str3 = "3qwerty";
+        String str4 = "4qwerty";
+        lst.add(str);
+        lst.add(str2);
+        lst.add(str3);
+        lst.add(str4);
+        System.out.println (lst);
+        Reverse(lst);
+        System.out.println (lst);
+    }
+
+    public static LinkedList<String> Reverse (LinkedList<String> lst){
+        String temp;
+        int leng = lst.size();
+        for (int i=0; i<(leng/2); i++) {
+            temp = lst.get(i);
+            lst.set(i, lst.get(leng-1-i));
+            lst.set((leng-1-i), temp);
+        }
+        return lst;
+    }
+}
